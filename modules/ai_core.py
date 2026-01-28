bf9c873d8844c424089e3e15b5229d46e85043ae
antinaosDev <alain.antinao.s@gamil.com>
2026-01-28 18:08:15 -0300

    .

--- modules/ai_core.py
+++ modules/ai_core.py
@@ -267,7 +267,7 @@ def analyze_document_vision(text_content, images_base64=[]):
             messages=[
                 {"role": "user", "content": user_content}
             ],
-            model="llama-3.2-90b-vision-preview",
+            model="llama-4-scout-17b-16e-instruct",
             temperature=0.1,
             max_tokens=4096
         )
